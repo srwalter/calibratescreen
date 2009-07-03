3effc71f8148f2c67b0747152cf76fb53128e355
Steven Walter <stevenrwalter@gmail.com>
2009-07-03 14:58:09 -0400

    Implement slope-intersect calibration algorithm

--- src/org/isageek/dasbrennen/CalibrateScreen/Calibrator.java
+++ src/org/isageek/dasbrennen/CalibrateScreen/Calibrator.java
@@ -7,6 +7,11 @@ import android.view.View.OnTouchListener;
 import android.widget.Toast;
 
 public class Calibrator implements OnTouchListener {
+	private final int TL_X = 0;
+	private final int TL_Y = 0;
+	private final int BR_X = 0;
+	private final int BR_Y = 0;
+	
 	private Context ctx;
 	
 	private class XY {
@@ -41,6 +46,26 @@ public class Calibrator implements OnTouchListener {
 		return false;
 	}
 	
+	private class ComputeMinMax {
+		public int new_min;
+		public int new_max;
+		
+		public ComputeMinMax(float curmin, float curmax, float e1, float e2, float t1, float t2, float max) {
+			/* Convert coordinate to touch values */
+			e1 = e1 * (curmax - curmin);
+			e1 = e1 + curmin;
+			e2 = e2 * (curmax - curmin);
+			e2 = e2 + curmin;
+			
+			/* Get our linear equation */
+			float m = (e2 - e1) / (t2 - t1);
+			float b = e1 - m*t1;
+			
+			new_min = (int)b;
+			new_max = (int)(m * max + b);
+		}
+	}
+	
 	private void recalibrate() {
 		if (topleft == null)
 			return;
@@ -48,9 +73,32 @@ public class Calibrator implements OnTouchListener {
 			return;
 		
 		int duration = Toast.LENGTH_LONG;
-		CharSequence msg = "recalibrate!";
+		CharSequence msg = "Recalibrated!";
 		Toast t = Toast.makeText(ctx, msg, duration);
 		t.show();
+	
+		/* XXX: read from sysfs */
+		int xmin = 100;
+		int xmax = 600;
+		
+		topleft.x = topleft.x * 240 / 320;
+		bottomright.x = bottomright.x * 240 / 320;
+		ComputeMinMax cmm = new ComputeMinMax(xmin, xmax, topleft.x, bottomright.x, TL_X, BR_X, 240);
+		xmin = cmm.new_min;
+		xmax = cmm.new_max;
+		
+		/* XXX: write to sysfs */
+		
+		int ymin = 50;
+		int ymax = 1000;
+		
+		topleft.y = topleft.y * 320 / 480;
+		bottomright.y = bottomright.y * 320 / 480;
+		cmm = new ComputeMinMax(ymin, ymax, topleft.y, bottomright.y, TL_Y, BR_Y, 320);
+		ymin = cmm.new_min;
+		ymax = cmm.new_max;
+		
+		/* XXX: write to sysfs */
 	}
 
 }
