ada5211e019644c282b00060305e469f9d8a3eed
Steven Walter <stevenrwalter@gmail.com>
2009-07-04 00:01:03 -0400

    Getting correct calibration values out
    Values still not correctly stored back to sysfs

--- src/org/isageek/dasbrennen/CalibrateScreen/Calibrator.java
+++ src/org/isageek/dasbrennen/CalibrateScreen/Calibrator.java
@@ -1,5 +1,10 @@
 package org.isageek.dasbrennen.CalibrateScreen;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 import android.content.Context;
 import android.view.MotionEvent;
 import android.view.View;
@@ -7,10 +12,11 @@ import android.view.View.OnTouchListener;
 import android.widget.Toast;
 
 public class Calibrator implements OnTouchListener {
-	private final int TL_X = 0;
-	private final int TL_Y = 0;
-	private final int BR_X = 0;
-	private final int BR_Y = 0;
+	/* These are the "screen" (240x320) coordinates of the targets */
+	private final int TL_X = 17;
+	private final int TL_Y = 41;
+	private final int BR_X = 216;
+	private final int BR_Y = 304;
 	
 	private Context ctx;
 	
@@ -32,6 +38,11 @@ public class Calibrator implements OnTouchListener {
 		float x = event.getRawX();
 		float y = event.getRawY();
 		
+		int duration = Toast.LENGTH_LONG;
+		CharSequence msg = "touch " + String.valueOf(x) + " " + String.valueOf(y);
+		Toast t = Toast.makeText(ctx, msg, duration);
+		t.show();
+		
 		XY coord = new XY();
 		coord.x = (int) x;
 		coord.y = (int) y;
@@ -52,18 +63,57 @@ public class Calibrator implements OnTouchListener {
 		
 		public ComputeMinMax(float curmin, float curmax, float e1, float e2, float t1, float t2, float max) {
 			/* Convert coordinate to touch values */
-			e1 = e1 * (curmax - curmin);
+			e1 = e1 * (curmax - curmin) / max;
 			e1 = e1 + curmin;
-			e2 = e2 * (curmax - curmin);
+			e2 = e2 * (curmax - curmin) / max;
 			e2 = e2 + curmin;
 			
 			/* Get our linear equation */
 			float m = (e2 - e1) / (t2 - t1);
 			float b = e1 - m*t1;
 			
+			if (b < 0)
+				b = 0;
+			
 			new_min = (int)b;
 			new_max = (int)(m * max + b);
 		}
+	}
+	
+	private int getSysfs(String filename) {
+		int val = 0;
+		try {
+			FileReader f = new FileReader(filename);
+			Scanner s = new Scanner(f);
+			val = s.nextInt();
+		} catch (IOException e) {
+			int duration = Toast.LENGTH_LONG;
+			CharSequence msg = "read fail!";
+			Toast t = Toast.makeText(ctx, msg, duration);
+			t.show();
+		}
+		int duration = Toast.LENGTH_LONG;
+		CharSequence msg = "read " + String.valueOf(val);
+		Toast t = Toast.makeText(ctx, msg, duration);
+		t.show();
+		return val;
+	}
+	
+	private void setSysfs(String filename, int val) {
+		try {
+			FileWriter f = new FileWriter(filename);
+			f.write(String.valueOf(val));
+			f.write("\n");
+		} catch (IOException e) {
+			int duration = Toast.LENGTH_LONG;
+			CharSequence msg = "write fail!";
+			Toast t = Toast.makeText(ctx, msg, duration);
+			t.show();
+		}
+		int duration = Toast.LENGTH_LONG;
+		CharSequence msg = "write " + String.valueOf(val);
+		Toast t = Toast.makeText(ctx, msg, duration);
+		t.show();
 	}
 	
 	private void recalibrate() {
@@ -72,33 +122,32 @@ public class Calibrator implements OnTouchListener {
 		if (bottomright == null)
 			return;
 		
-		int duration = Toast.LENGTH_LONG;
-		CharSequence msg = "Recalibrated!";
-		Toast t = Toast.makeText(ctx, msg, duration);
-		t.show();
-	
-		/* XXX: read from sysfs */
-		int xmin = 100;
-		int xmax = 600;
+		/* Compute XMIN and XMAX */
+		int xmin = getSysfs("/sys/class/vogue_ts/xmin");
+		int xmax = getSysfs("/sys/class/vogue_ts/xmax");
 		
 		topleft.x = topleft.x * 240 / 320;
 		bottomright.x = bottomright.x * 240 / 320;
 		ComputeMinMax cmm = new ComputeMinMax(xmin, xmax, topleft.x, bottomright.x, TL_X, BR_X, 240);
-		xmin = cmm.new_min;
-		xmax = cmm.new_max;
 		
-		/* XXX: write to sysfs */
+		setSysfs("/sys/class/vogue_ts/xmin", cmm.new_min);
+		setSysfs("/sys/class/vogue_ts/xmax", cmm.new_max);
 		
-		int ymin = 50;
-		int ymax = 1000;
+		/* Compute YMIN and YMAX */
+		int ymin = getSysfs("/sys/class/vogue_ts/ymin");
+		int ymax = getSysfs("/sys/class/vogue_ts/ymax");
 		
 		topleft.y = topleft.y * 320 / 480;
 		bottomright.y = bottomright.y * 320 / 480;
 		cmm = new ComputeMinMax(ymin, ymax, topleft.y, bottomright.y, TL_Y, BR_Y, 320);
-		ymin = cmm.new_min;
-		ymax = cmm.new_max;
 		
-		/* XXX: write to sysfs */
+		setSysfs("/sys/class/vogue_ts/ymin", cmm.new_min);
+		setSysfs("/sys/class/vogue_ts/ymax", cmm.new_max);
+		
+		int duration = Toast.LENGTH_LONG;
+		CharSequence msg = "Recalibrated!";
+		Toast t = Toast.makeText(ctx, msg, duration);
+		t.show();
 	}
 
 }
